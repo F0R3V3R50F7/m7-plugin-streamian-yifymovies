3b509dd8b804e327b67ece4cce576eb843b286e9
F0R3V3R50F7 <Josh1092@yandex.com>
2024-12-18 17:46:44 +0000

    Create YifyMoviesDev.js

---
+++ YifyMoviesDev.js
@@ -0,0 +1,27 @@
+page.loading = true;
+var relevantTitlePartMatch = title.match(/\s(S\d{2}E\d{2})/i);
+if (relevantTitlePartMatch) {
+    console.log('Yify Movies | Show detected, skipping...');
+    return [];
+}
+
+var apiUrl = "https://yts.mx/api/v2/list_movies.json?query_term=" + encodeURIComponent(title);
+var response = http.request(apiUrl);
+var json = JSON.parse(response);
+if (json.data && json.data.movies && json.data.movies.length > 0) {
+    var results = [];
+    json.data.movies.forEach(function (movie) {
+        movie.torrents.forEach(function (torrent) {
+            var seederCount = torrent.seeds;
+            var magnetLink = torrent.url;
+            var item = magnetLink + " - " + '' + " - " + seederCount;
+            results.push(item);
+        });
+    });
+    page.loading = false;
+    return results;
+} else {
+    //page.appendItem("", "message", { title: "No results found on YTS for " + decodedTitle });
+    page.loading = false;
+    return [];
+}
